203c08d386fd83f4770f18fe6645370617e45106
Badr <choubai.badr@protonmail.com>
2024-03-27 22:18:40 -0600

    lint and format bmi.py

--- assignments/assignment-problem/python-app/bmi.py
+++ assignments/assignment-problem/python-app/bmi.py
@@ -2,16 +2,16 @@ print('(1) Metric (m, kg) or (2) Non-Metric (ft, pounds)?')
 
 chosen_system = input('Please choose: ')
 
-if (chosen_system != '1' and chosen_system != '2'):
-  print('You have to choose either metric or non-metric. Shutting down...')
-  exit()
+if chosen_system != '1' and chosen_system != '2':
+    print('You have to choose either metric or non-metric. Shutting down...')
+    exit()
 
 height_unit = 'meters'
 weight_unit = 'kilograms'
 
 if (chosen_system == '2'):
-  height_unit = 'feet'
-  weight_unit = 'pounds'
+    height_unit = 'feet'
+    weight_unit = 'pounds'
 
 print('Please enter your height in ' + height_unit)
 user_height = float(input('Your height: '))
@@ -22,9 +22,9 @@ user_weight = float(input('Your weight: '))
 adj_height = user_height
 adj_weight = user_weight
 
-if (chosen_system == '2'):
-  adj_height = user_height / 3.28084
-  adj_weight = user_weight / 2.20462
+if chosen_system == '2':
+    adj_height = user_height / 3.28084
+    adj_weight = user_weight / 2.20462
 
 bmi = adj_weight / (adj_height * adj_height)
 
