308f8a6b9df298b77f60b9537478123f962496e4
Badr <choubai.badr@protonmail.com>
2024-03-28 15:14:20 -0600

    add scripts to .dockerignore

--- managing-data-volumes/project/start-container.sh
+++ managing-data-volumes/project/start-container.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:80 -d --name feedback-app --rm -v $(pwd):/app:ro -v /app/temp -v /app/node_modules -v feedback:/app/feedback docker-tutorial-volumes
+docker run -p 8080:80 -d --name feedback-app --rm -v $(pwd):/app -v /app/temp -v /app/node_modules -v feedback:/app/feedback docker-tutorial-volumes
