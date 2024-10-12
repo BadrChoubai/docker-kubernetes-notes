8fd6f201633d3729877e6aab1fe842d0e73922b5
Badr <choubai.badr@protonmail.com>
2024-10-11 19:44:40 -0600

    Add new endpoint: /health

--- Kubernetes/01-first-deployment/app/app.js
+++ Kubernetes/01-first-deployment/app/app.js
@@ -4,9 +4,15 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
+    <h1>Hello from this Node.js app!</h1>
     <p>Try sending a request to /error and see what happens</p>
   `);
+});
+
+app.get('/health', (req, res) => {
+  res
+      .status(200)
+      .json("Healthy");
 });
 
 app.get('/error', (req, res) => {
