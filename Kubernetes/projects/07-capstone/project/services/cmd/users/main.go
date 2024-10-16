65269bf2f931e9850bd80a1c154626e81983b976
Badr <choubai.badr@protonmail.com>
2024-10-16 16:50:14 -0600

    refactor go code and add golangci-lint

--- Kubernetes/projects/07-capstone/project/services/cmd/users/main.go
+++ Kubernetes/projects/07-capstone/project/services/cmd/users/main.go
@@ -2,96 +2,14 @@ package main
 
 import (
 	"context"
-	"errors"
 	"log"
-	"net"
-	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
-	"strings"
 	"syscall"
 	"time"
+
+	"github.com/badrchoubai/services/internal/server"
 )
-
-type (
-	Server interface {
-		Serve() error
-		Shutdown(ctx context.Context) error
-	}
-
-	server struct {
-		ctx        context.Context
-		httpServer *http.Server
-	}
-)
-
-// addRoutes is where the entire API surface is mapped
-// https://grafana.com/blog/2024/02/09/how-i-write-http-services-in-go-after-13-years/#map-the-entire-api-surface-in-routesgo
-func addRoutes(mux *http.ServeMux) {
-	mux.Handle("/*", http.NotFoundHandler())
-}
-
-// Heartbeat logs incoming requests on global HTTP handler
-func heartbeat(next http.Handler, endpoint string) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if (r.Method == "GET" || r.Method == "HEAD") && strings.EqualFold(r.URL.Path, endpoint) {
-			w.Header().Set("Content-Type", "text/plain")
-			w.WriteHeader(http.StatusOK)
-			w.Write([]byte("."))
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func NewRouter() http.Handler {
-	mux := http.NewServeMux()
-
-	addRoutes(mux)
-
-	var handler http.Handler = mux
-	handler = heartbeat(handler, "/health")
-
-	return handler
-}
-
-func (s *server) Shutdown(ctx context.Context) error {
-	return s.httpServer.Shutdown(ctx)
-}
-
-func (s *server) Serve() error {
-	if err := s.httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		return err
-	}
-
-	return nil
-}
-
-func newServer(ctx context.Context, handler http.Handler) Server {
-	const (
-		maxTimeout   = 120 * time.Second
-		readTimeout  = 5 * time.Second
-		writeTimeout = 2 * readTimeout
-		idleTimeout  = maxTimeout
-	)
-
-	httpserver := &http.Server{
-		Addr:         net.JoinHostPort("0.0.0.0", strconv.Itoa(8080)),
-		Handler:      handler,
-		ReadTimeout:  readTimeout,
-		WriteTimeout: writeTimeout,
-		IdleTimeout:  idleTimeout,
-	}
-
-	srv := &server{
-		ctx:        ctx,
-		httpServer: httpserver,
-	}
-
-	return srv
-}
 
 func run(ctx context.Context) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
@@ -99,8 +17,8 @@ func run(ctx context.Context) error {
 
 	infoLog := log.New(os.Stdout, "", log.LstdFlags|log.Lmicroseconds)
 	errLog := log.New(os.Stderr, "", log.LstdFlags|log.Lmicroseconds)
-	router := NewRouter()
-	srv := newServer(ctx, router)
+	router := server.NewRouter()
+	srv := server.NewServer(ctx, router)
 
 	var serveError error
 
